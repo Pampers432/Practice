593f3edf9c5791018b547f217cdd9efb2723affe
Pampers432 <max23012007@gmail.com>
2024-11-02 20:01:44 +0300

    Added few descriptors

--- HandBook/Classes/Car.cs
+++ HandBook/Classes/Car.cs
@@ -1,13 +1,15 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace HandBook.Classes
 {
     internal class Car
     {
+        public int Id { get; set; }
         public string brand { get; set; }
         public string color { get; set; }
         public string serialNumber { get; set; }
@@ -16,5 +18,51 @@ namespace HandBook.Classes
         public string featuresOfDesignAndColoring { get; set; }
         public string dateofLastTechnicalInspection { get; set; }
         public string ownersPassportDetails { get; set; }
+
+        public string SerialNumber
+        {
+            get {  return serialNumber; }
+            set
+            {
+                string vinPattern = @"^[A-HJ-NPR-Z0-9]{17}$";
+                while (true)
+                {
+                    if (Regex.IsMatch(value, vinPattern))
+                    {
+                        serialNumber = value;
+                        break;
+                    }
+                    else
+                    {
+                        throw new FormatException("Некорректный заводской номер, попробуйте ещё раз.");
+                    }
+                }
+                
+            }
+        }
+
+        public string SideNumber
+        {
+            get { return sideNumber; }
+            set
+            {
+                while (true)
+                {
+                    string sideNumberPattern = @"^[A-Za-z0-9]+$"; 
+                    if (Regex.IsMatch(value, sideNumberPattern))
+                    {
+                        sideNumber = value;
+                    }
+                    else
+                    {
+                        throw new FormatException("Некорректный бортовой номер, попробуйте ещё раз.");
+                    }
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return String.Format($"ID: {Id}, Марка: {brand}, Цвет: {color}, VIN: {serialNumber}, Бортовой номер: {sideNumber}, Дата выпуска: {dateOfManufacture}, Особенности конструкции: {featuresOfDesignAndColoring}, Дата последнего техосмотра: {dateofLastTechnicalInspection}, Паспортные данные владельца: {ownersPassportDetails}");
+        }
     }
 }
