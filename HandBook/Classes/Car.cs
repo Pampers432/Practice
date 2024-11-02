3f6eca0a6a1a54a226957260cf407c60428b0035
Pampers432 <max23012007@gmail.com>
2024-11-02 19:33:33 +0300

    Create Car.cs
    Class car

---
+++ HandBook/Classes/Car.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandBook.Classes
+{
+    internal class Car
+    {
+        public string brand { get; set; }
+        public string color { get; set; }
+        public string serialNumber { get; set; }
+        public string sideNumber { get; set; }
+        public string dateOfManufacture { get; set; }
+        public string featuresOfDesignAndColoring { get; set; }
+        public string dateofLastTechnicalInspection { get; set; }
+        public string ownersPassportDetails { get; set; }
+    }
+}
