1f8e203311abbb9ba32f815b6c83b1f3baacca3f
Pampers432 <max23012007@gmail.com>
2024-11-03 12:40:16 +0300

    Add files via upload
    Добавил дескрипторы для всех требующих того полей
    Добавил конструктор
    Добавил метод ToString

--- HandBook/Classes/Car.cs
+++ HandBook/Classes/Car.cs
@@ -12,32 +12,27 @@ namespace HandBook.Classes
         public int Id { get; set; }
         public string brand { get; set; }
         public string color { get; set; }
-        public string serialNumber { get; set; }
-        public string sideNumber { get; set; }
-        public string dateOfManufacture { get; set; }
+        private string serialNumber { get; set; }
+        private string sideNumber { get; set; }
+        private DateTime dateOfManufacture { get; set; }
         public string featuresOfDesignAndColoring { get; set; }
-        public string dateofLastTechnicalInspection { get; set; }
-        public string ownersPassportDetails { get; set; }
+        private DateTime dateofLastTechnicalInspection { get; set; }
 
+        // Descriptors
         public string SerialNumber
         {
             get {  return serialNumber; }
             set
             {
                 string vinPattern = @"^[A-HJ-NPR-Z0-9]{17}$";
-                while (true)
+                if (Regex.IsMatch(value, vinPattern))
                 {
-                    if (Regex.IsMatch(value, vinPattern))
-                    {
-                        serialNumber = value;
-                        break;
-                    }
-                    else
-                    {
-                        throw new FormatException("Некорректный заводской номер, попробуйте ещё раз.");
-                    }
+                    serialNumber = value;
                 }
-                
+                else
+                {
+                    throw new FormatException("Некорректный заводской номер, попробуйте ещё раз.");
+                }
             }
         }
 
@@ -46,23 +41,76 @@ namespace HandBook.Classes
             get { return sideNumber; }
             set
             {
-                while (true)
+                string sideNumberPattern = @"^[A-Za-z0-9]+$";
+                if (Regex.IsMatch(value, sideNumberPattern))
                 {
-                    string sideNumberPattern = @"^[A-Za-z0-9]+$"; 
-                    if (Regex.IsMatch(value, sideNumberPattern))
-                    {
-                        sideNumber = value;
-                    }
-                    else
-                    {
-                        throw new FormatException("Некорректный бортовой номер, попробуйте ещё раз.");
-                    }
+                    sideNumber = value;
+                }
+                else
+                {
+                    throw new FormatException("Некорректный бортовой номер, попробуйте ещё раз.");
                 }
             }
         }
+
+        public string DateOfManufacture
+        {
+            get { return dateOfManufacture.ToString("dd.MM.yyyy"); }
+            set
+            {
+                if (DateTime.TryParseExact(value, "dd.MM.yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime parsedDate))
+                {
+                    dateOfManufacture = parsedDate;
+                }
+                else
+                {
+                    throw new FormatException("Некорректный формат даты выпуска, используйте формат DD.MM.YYYY");
+                }
+            }
+        }
+
+        public string DateofLastTechnicalInspection
+        {
+            get { return dateofLastTechnicalInspection.ToString("dd.MM.yyyy"); }
+            set
+            {
+                if (DateTime.TryParseExact(value, "dd.MM.yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime parsedDate))
+                {
+                    dateofLastTechnicalInspection = parsedDate;
+                }
+                else
+                {
+                    throw new FormatException("Некорректный формат даты выпуска, используйте формат DD.MM.YYYY");
+                }
+            }
+        }
+
+        // Constructors
+        public Car(int id, string brand, string color, string serialNumber, string sideNumber, string dateOfManufacture, string featuresOfDesignAndColoring, string dateofLastTechnicalInspection)
+        {
+            Id = id;
+            this.brand = brand;
+            this.color = color;
+            SerialNumber = serialNumber;
+            SideNumber = sideNumber;
+            DateOfManufacture = dateOfManufacture;
+            this.featuresOfDesignAndColoring = featuresOfDesignAndColoring;
+            DateofLastTechnicalInspection = dateofLastTechnicalInspection;
+        }
+
+        // Methods
         public override string ToString()
         {
-            return String.Format($"ID: {Id}, Марка: {brand}, Цвет: {color}, VIN: {serialNumber}, Бортовой номер: {sideNumber}, Дата выпуска: {dateOfManufacture}, Особенности конструкции: {featuresOfDesignAndColoring}, Дата последнего техосмотра: {dateofLastTechnicalInspection}, Паспортные данные владельца: {ownersPassportDetails}");
+            return $"{"ID:",-30} {Id}\n" +
+                   $"{"Марка:",-30} {brand}\n" +
+                   $"{"Цвет:",-30} {color}\n" +
+                   $"{"VIN:",-30} {SerialNumber}\n" +
+                   $"{"Бортовой номер:",-30} {SideNumber}\n" +
+                   $"{"Дата выпуска:",-30} {DateOfManufacture:dd.MM.yyyy}\n" +
+                   $"{"Особенности конструкции:",-30} {featuresOfDesignAndColoring}\n" +
+                   $"{"Дата последнего техосмотра:",-30} {DateofLastTechnicalInspection}";
         }
+
+
     }
 }
